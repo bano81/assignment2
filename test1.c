7c24204098f403553fc989f01f1236484a633fa3
bano81 <baqernour@live.dk>
2025-10-08 23:08:43 +0200

    new test1 with 3 threads

--- test1.c
+++ test1.c
@@ -20,20 +20,32 @@
 static AlarmQueue q;
 
 void * producer (void * arg) {
-  msleep(500);
   put_alarm(q, 42);
   put_alarm(q, 43);
-  msleep(500);
-  put_normal(q, 1); 
+  put_normal(q, 1);
+  return 0;
+}
+
+void * producer2 (void * arg) {
+  msleep(200);
+  put_alarm(q, 45);
+  put_normal(q, 2);
   return 0;
 }
 
 void * consumer(void * arg) {
+  msleep(1000);
   get(q);
+  msleep(500);
+  get(q);
+  msleep(500);
+  get(q);
+  msleep(500);
   get(q);
   get(q);
   return 0;
 }
+
 
 int main(int argc, char ** argv) {
     int ret;
@@ -45,25 +57,26 @@ int main(int argc, char ** argv) {
     exit(1);
   }
   
-  pthread_t t1;
-  pthread_t t2;
+  pthread_t t1, t2, t3 ;
 
-  void * res1;
-  void * res2;
+
+  void *res1, *res2, *res3;
 
   printf("----------------\n");
 
   /* Fork threads */
   pthread_create(&t1, NULL, producer, NULL);
-  pthread_create(&t2, NULL, consumer, NULL);
+  pthread_create(&t2, NULL, producer2, NULL);
+  pthread_create(&t3, NULL, consumer, NULL);
   
   /* Join with all threads */
   pthread_join(t1, &res1);
   pthread_join(t2, &res2);
+  pthread_join(t3, &res3);
 
   printf("----------------\n");
-  printf("Threads terminated with %ld, %ld\n", (uintptr_t) res1, (uintptr_t) res2);
-
+  //printf("Threads terminated with %ld, %ld\n", (uintptr_t) res1, (uintptr_t) res2);
+  printf("Threads terminated with %ld, %ld, %ld\n", (uintptr_t) res1, (uintptr_t) res2, (uintptr_t) res3);
   print_sizes(q);
   
   return 0;
